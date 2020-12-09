b060b0a4836383cf774f6a7c71e0b6f2a21a7948
Liam Snowdon <snowdon.liam@gmail.com>
2020-12-09 03:52:41 +0000

    Add date suffix for long dates

--- src/utilities.js
+++ src/utilities.js
@@ -1,16 +1,33 @@
 export default class Utilities {
-    static longDate (date) {
-        if (!date) return '';
+  static longDate (date) {
+    if (!date) return '';
 
-        if (typeof date === 'string') {
-            date = new Date(date);
-        }
-    
-        const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-        const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', "December"];
-    
-        return `${days[date.getDay()]} ${date.getDate()} ${months[date.getMonth()]} ${date.getFullYear()}`;
+    if (typeof date === 'string') {
+      date = new Date(date);
     }
+  
+    const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+    const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', "December"];
+  
+    return `${days[date.getDay()]} ${date.getDate()}${dateSuffix(date.getDate())} ${months[date.getMonth()]} ${date.getFullYear()}`;
+
+    function dateSuffix (date) {
+      switch (date) {
+        case 1:
+        case 21:
+        case 31:
+          return "st";
+        case 2:
+        case 22:
+          return "nd";
+        case 3:
+        case 23:
+          return "rd";
+        default:
+          return "th";
+      };
+    }
+  }
 
     static shortDate (date) {
         if (!date) return '';
